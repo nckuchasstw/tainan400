fa91afe0a8db252fa202eef1d4056172376207c1
wei <mimi19910930@gmail.com>
2023-07-26 21:15:31 +0800

    更新messenger

--- all.js
+++ all.js
@@ -243,22 +243,19 @@ gsap.to(".navbar",{
     var chatbox = document.getElementById('fb-customer-chat');
     chatbox.setAttribute("page_id", "104877438580876");
     chatbox.setAttribute("attribution", "biz_inbox");
- 
 
-    <!-- Your SDK code -->
-   
-      window.fbAsyncInit = function() {
-        FB.init({
-          xfbml            : true,
-          version          : 'v17.0'
-        });
-      };
+    window.fbAsyncInit = function() {
+      FB.init({
+        xfbml            : true,
+        version          : 'v17.0'
+      });
+    };
 
-      (function(d, s, id) {
-        var js, fjs = d.getElementsByTagName(s)[0];
-        if (d.getElementById(id)) return;
-        js = d.createElement(s); js.id = id;
-        js.src = 'https://connect.facebook.net/zh_TW/sdk/xfbml.customerchat.js';
-        fjs.parentNode.insertBefore(js, fjs);
-      }(document, 'script', 'facebook-jssdk'));
-    
+    (function(d, s, id) {
+      var js, fjs = d.getElementsByTagName(s)[0];
+      if (d.getElementById(id)) return;
+      js = d.createElement(s); js.id = id;
+      js.src = 'https://connect.facebook.net/zh_TW/sdk/xfbml.customerchat.js';
+      fjs.parentNode.insertBefore(js, fjs);
+    }(document, 'script', 'facebook-jssdk'));
+  
