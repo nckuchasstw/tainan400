da47a8b7c6597358ddfa32cccdfa18015cefc90f
wei <mimi19910930@gmail.com>
2023-10-23 15:31:53 +0800

    修正

--- vuepaper.js
+++ vuepaper.js
@@ -223,7 +223,7 @@ const app = Vue.createApp({
       {title:'2023臺南國際人權藝術節',url:'https://www.tihrf-nckumuseum.com/',author:'---',img:'humanright.jpg',data:"2023.10~12",register:'開放報名中'},
       {title:'鬥鬧熱走唱隊',url:'activity17.html',author:'鬥鬧熱走唱隊',img:'https://www.tihrf-nckumuseum.com/wp-content/uploads/2023/09/%E9%AC%A5%E9%AC%A7%E7%86%B1%E8%B5%B0%E5%94%B1%E9%9A%8A%E6%B5%B7%E5%A0%B1-3-1085x1536.png',data:"2023.10.14",register:'活動已結束'},
       {title:'「府城飲食文化的趣味」專題演講',url:'activity05.html',author:'魚夫',img:'231019.jpg',data:"2023.10.19",register:'活動已結束'},
-      {title:'詩光墨影——黃華安成大文學院之美個展',url:'activity18.html',author:'--',img:'1121030.jpg',data:"2023.10.30~11.14",register:'自由入場'},
+      {title:'詩光墨影——黃華安成大文學院之美個展',url:'activity18.html',author:'--',img:'20231030.jpg',data:"2023.10.30~11.14",register:'自由入場'},
       {title:'「臺南400」演唱會',url:'activity11.html',author:'謝銘祐、大支、阿雞、韓森',img:'1121108.jpg',data:"2023.11.08",register:'開放報名中'},
       // {title:'「展．望」國際前瞻論壇',url:'activity14.html',author:'--',img:'1110.jpg',data:"2023.11.10",register:'尚未開放報名'},
       // {title:'國際工作坊',url:'',author:'--',img:'mainposter.jpg',data:"2023.11.11",register:'尚未開放報名'},
