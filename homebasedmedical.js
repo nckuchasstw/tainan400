d9748622f3b8ffe5a049cef4d89c557ae426c9e4
wei <mimi19910930@gmail.com>
2024-06-25 14:09:50 +0800

    更新

--- homebasedmedical.js
+++ homebasedmedical.js
@@ -12,7 +12,7 @@ const app = Vue.createApp({
 
           {unit:'在宅醫療學會理事長、台東都蘭診所所長',url:'https://www.clinicdulan.com/team01',name:'余尚儒',ename:'Sang-Ju Yu',img:'余尚儒.png',field:'在宅醫療',cv1:'高雄醫學大學醫學系博士',cv2:'發表人Presenter'},
 
-          {unit:'國立成功大學醫學系心臟血管外科主任兼任醫學系系主任',url:'https://dseas.ncnu.edu.tw/p/406-1030-3646,r234.php?Lang=zh-tw',name:'阮俊能',ename:'Jun-Neng Roan,',img:'阮俊能.png',field:'心臟衰竭、心臟移植、葉克膜、血管生理、肺高壓、動靜脈廔管',cv1:'國立成功大學臨床醫學研究所博士',cv2:'發表人Presenter'},
+          {unit:'國立成功大學醫學系心臟血管外科主任兼任醫學系系主任',url:'https://dseas.ncnu.edu.tw/p/406-1030-3646,r234.php?Lang=zh-tw',name:'阮俊能',ename:'Jun-Neng Roan',img:'阮俊能.png',field:'心臟衰竭、心臟移植、葉克膜、血管生理、肺高壓、動靜脈廔管',cv1:'國立成功大學臨床醫學研究所博士',cv2:'發表人Presenter'},
 
           {unit:'國立成功大學醫學系人文暨社會醫學科教授兼主任',url:'https://researchoutput.ncku.edu.tw/zh/persons/hsiuyun-wang',name:'王秀雲',ename:'Hsiuyun Wang',img:'王秀雲.png',field:'近代醫學史、科學史、台灣戰後醫學史、性別研究',cv1:'美國威斯康辛大學麥迪遜分校科學史系博士',cv2:'發表人Presenter'},
   
