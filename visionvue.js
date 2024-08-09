74ad704e7dfdc0e3a4f3dea05513a5933d27c399
wei <mimi19910930@gmail.com>
2024-08-09 11:10:45 +0800

    更新

--- visionvue.js
+++ visionvue.js
@@ -17,7 +17,7 @@ const app = Vue.createApp({
         //   {unit:'國立成功大學歷史系教授兼副校長',url:'https://radb.ncku.edu.tw/Personal_Report/profile.php?s=ODUwODA1NCA7YWxsOzsxO3BlcnNvbmFsX3JlcG9ydF9jc3NfMQ==',name:'陳玉女',ename:'Yuh-Neu Chen',img:'陳玉女.png',field:'明代社會史、明代佛教史、中國佛教史',cv1:'日本國立九州大學東洋史學博士',cv2:'主持人Moderator'},
 
   
-          {unit:'加拿大渥太華大學社會學暨人類學專業學院教授',url:'https://www.uottawa.ca/research-innovation/hrrec/people/simon-scott',name:'史國良',ename:'Scott E. Simon',img:'史國良.png',field:'台灣原住民族（太魯閣族、賽德克族，達悟族）社會、自治與主權、原住民族的人權、原住民與國家的關係、關係性 (relationality)',cv1:'Ph.D., Anthropologie (Université McGill, Montréal)',cv2:'發表人Presenter'},
+          {unit:'加拿大渥太華大學社會學暨人類學專業學院教授',url:'https://www.uottawa.ca/research-innovation/hrrec/people/simon-scott',name:'史國良',ename:'Scott E. Simon',img:'史國良.png',field:'臺灣原住民族（太魯閣族、賽德克族，達悟族）社會、自治與主權、原住民族的人權、原住民與國家的關係、關係性 (relationality)',cv1:'Ph.D., Anthropologie (Université McGill, Montréal)',cv2:'發表人Presenter'},
   
           {unit:'國立成功大學體育健康與休閒研究所教授',url:'https://pe-acad.ncku.edu.tw/p/404-1045-85805.php?Lang=zh-tw',name:'徐珊惠',ename:'Tiffany Hsu',img:'徐珊惠.png',field:'桌球、游泳、羽球、課程設計、質化研究、性別研究',cv1:'美國喬治亞大學人體運動學研究哲學博士',cv2:'主持人Moderator'},   
     
@@ -36,7 +36,7 @@ const app = Vue.createApp({
   
           {unit:'法國里昂大學政治學院政治學系副教授',url:'https://www.asiatheque.com/en/contributor/stephane-corcuff',name:'高格孚',ename:'Stéphane Corcuff',img:'高格孚.png',field:'臺灣認同政治、臺灣史以及兩岸地緣政治、台海關係',cv1:'法國巴黎政治學院政治學博士',cv2:'發表人Presenter'},
   
-          {unit:'中央研究院臺灣史研究所副研究員',url:'https://www.ith.sinica.edu.tw/members_faculty_look.php?l=c&no=3&id=76&page=1&ps=30',name:'吳叡人',ename:'Rwei-ren Wu',img:'吳叡人.png',field:'比較政治、政治理論、亞洲民族主義研究、台灣政治史、台灣政治思想史、日本近現代政治史、日本近現代政治思想史',cv1:'美國芝加哥大學政治學博士',cv2:'發表人Presenter'},
+          {unit:'中央研究院臺灣史研究所副研究員',url:'https://www.ith.sinica.edu.tw/members_faculty_look.php?l=c&no=3&id=76&page=1&ps=30',name:'吳叡人',ename:'Rwei-ren Wu',img:'吳叡人.png',field:'比較政治、政治理論、亞洲民族主義研究、臺灣政治史、臺灣政治思想史、日本近現代政治史、日本近現代政治思想史',cv1:'美國芝加哥大學政治學博士',cv2:'發表人Presenter'},
   
           {unit:'國立中興大學國際政治研究所特聘教授',url:'https://gioip.nchu.edu.tw/Front/Faculty/Full-Time/Members.aspx?id=IXkBZ6GdMlI=&PID=0000000005',name:'蔡東杰',ename:'Tung-Chieh Tsai ',img:'蔡東杰.png',field:'近代國際關係史、當前中國外交政策、東亞區域結構變遷發展、第三世界發展問題研究、兩岸關係問題研究',cv1:'國立政治大學政治研究所博士',cv2:'發表人Presenter'},
   
