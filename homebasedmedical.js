d970cb03a8dfb0666c80272ef7cc1e5ae6bd2468
wei <mimi19910930@gmail.com>
2024-09-06 08:36:01 +0800

    更新

--- homebasedmedical.js
+++ homebasedmedical.js
@@ -4,7 +4,7 @@ const app = Vue.createApp({
       return {
         text:'文字',
         activityList2025: [    
-          {unit:'國立成功大歷史學系副教授兼人文社會科學中心副主任',url:'https://researchoutput.ncku.edu.tw/zh/persons/hung-bin-hsu',name:'許宏彬',ename:'Hung Bin Hsu',img:'許宏彬.png',field:'醫療史、台灣史、科技與社會研究',cv1:'倫敦大學亞非學院歷史學博士',cv2:'主持人Moderator'},
+          {unit:'國立成功大歷史學系副教授兼人文社會科學中心副主任',url:'https://researchoutput.ncku.edu.tw/zh/persons/hung-bin-hsu',name:'許宏彬',ename:'Hung-Bin Hsu',img:'許宏彬.png',field:'醫療史、臺灣史、科技與社會研究',cv1:'倫敦大學亞非學院歷史學博士',cv2:'主持人Moderator'},
   
           {unit:'日本宮崎大學醫學部講座教授',url:'https://www.med.pref.miyazaki.lg.jp/backnumber/yoshimura/',name:'吉村學',ename:'Yoshimura Manabu',img:'吉村學.png',field:'家庭醫學',cv1:'宮崎醫科大學博士',cv2:' 專講keynote speaker'},
   
@@ -14,7 +14,7 @@ const app = Vue.createApp({
 
           {unit:'國立成功大學醫學系主任、心臟血管外科主任',url:'https://dseas.ncnu.edu.tw/p/406-1030-3646,r234.php?Lang=zh-tw',name:'阮俊能',ename:'Jun-Neng Roan',img:'阮俊能.png',field:'心臟衰竭、心臟移植、葉克膜、血管生理、肺高壓、動靜脈廔管',cv1:'國立成功大學臨床醫學研究所博士',cv2:'發表人Presenter'},
 
-          {unit:'國立成功大學醫學系人文暨社會醫學科教授兼主任',url:'https://researchoutput.ncku.edu.tw/zh/persons/hsiuyun-wang',name:'王秀雲',ename:'Hsiuyun Wang',img:'王秀雲.png',field:'近代醫學史、科學史、台灣戰後醫學史、性別研究',cv1:'美國威斯康辛大學麥迪遜分校科學史系博士',cv2:'發表人Presenter'},
+          {unit:'國立成功大學醫學系人文暨社會醫學科教授兼主任',url:'https://researchoutput.ncku.edu.tw/zh/persons/hsiuyun-wang',name:'王秀雲',ename:'Hsiuyun Wang',img:'王秀雲.png',field:'近代醫學史、科學史、臺灣戰後醫學史、性別研究',cv1:'美國威斯康辛大學麥迪遜分校科學史系博士',cv2:'發表人Presenter'},
   
         
   
