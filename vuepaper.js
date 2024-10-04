4e5996bc846c364d2b0cf62904fe254bd76a7413
wei <mimi19910930@gmail.com>
2024-10-04 10:39:44 +0800

    更新

--- vuepaper.js
+++ vuepaper.js
@@ -307,7 +307,7 @@ const app = Vue.createApp({
       
         {title:'「展．望」人社國際前瞻論壇',url:'envision.html',author:'--',img:'展望論壇.jpg',data:"2024.10.18",register:'開放報名中'},
 
-        {title:'「2024年美國總統大選與台美中關係初探」專題演講',url:'activity30.html',author:'高碩泰',img:'20241023.jpg',data:"2024.10.23",register:'活動尚未開始'},
+        {title:'「2024年美國總統大選與台美中關係初探」專題演講',url:'activity30.html',author:'高碩泰',img:'20241023.jpg',data:"2024.10.23",register:'開放報名中'},
 
 
         {title:'「偏鄉醫療與在地人才培育」座談會',url:'https://docs.google.com/forms/d/e/1FAIpQLSf3Zchx7kQeH8MT0xU_PAtKKsImZqvrI-aYlOc_2AJHy3guPA/viewform',author:'--',img:'https://chass.ncku.edu.tw/var/file/11/1011/img/1002-04.jpg',data:"2024.10.30",register:'開放報名中'},
